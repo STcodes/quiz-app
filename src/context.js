128b855dd1c9ac542c87dd1ba7d03b5a1ba3a1db
Samir <samirtahiri2310@gmail.com>
2022-01-31 23:45:57 +0100

    Deploy bugs

--- src/context.js
+++ src/context.js
@@ -9,9 +9,9 @@ const table = {
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
-// const url = ''
-// const tempUrl =
-//   'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+const url = ''
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
